ca5a31dbce932191a43254b176f71abece168416
Michael Ludvig <mludvig@logix.net.nz>
2017-12-07 14:17:32 +1300

    Improved metadata

--- ec2-start-stop.lambda.py
+++ ec2-start-stop.lambda.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
+# Very simple EC2 Start/Stop lambda function.
+# Author: Michael Ludvig @ aws.nz
+#
+# Expects 'event' input in JSON format:
+# {
+#    "instance_id": "i-1234567890abcdefgh",
+#    "action": "start" (or "stop")
+# }
+#
+# Best to call from AWS CloudWatch Events scheduler rule
+
 import boto3
 
 def lambda_handler(event, context):
-    ec2 = boto3.resource('ec2')
+    ec2 = boto3.client('ec2')
     try:
         instance_id = event['instance_id']
         action = event['action']
